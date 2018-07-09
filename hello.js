7962f600ab04900fcb42e36b80510f15c950d198
Student <student@Admins-MacBook-Air.local>
2018-07-09 12:01:41 -0700

    update

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
-// Author: FirstName LastName
+// Author: MOnserrac Franco
+
+console. log ('Hello Code Next !');
+console.log("It is a beautiful day for programming, isnt it? ")
+console.log ("Hey! What's 1+1?");
+console.log ("hey what is "+1+1+" "of couse!");
+console.log ("No, its" +(1+1) + "silly goose!" );
+console.log (This isn't going to get printed, is it");
