7ec16b74a8dff1a3b0b7cf3e9876ca5e80baee27
Student <student@Admins-MacBook-Air.local>
2018-07-10 11:28:58 -0700

    update

--- easter-predictor.js
+++ easter-predictor.js
@@ -1,1 +1,9 @@
 // Author: FirstName LastName
+let y = 2004;
+console.log("y = " + y);
+let a = y%19;
+console.log (" The value of a is " + a);
+let b = Math.floor (y / 100);
+console.log ("b is" + b);
+let c = (y%100);
+let d = Math.floor(b/4);
